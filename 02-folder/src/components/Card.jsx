f3a4f60e6dc867a379d2beecaa25cebc339ec3a2
Anshu kumari <anshuak6595@gmail.com>
2026-08-06 23:32:11 +0530

    Add another practice folder

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <h2>Anshu kumari</h2>
+        <p>I am learning React. My goal is full Stack developer</p>
+      </div>
+    </>
+  );
+};
+export default Card;
---
+++ 02-folder/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <ul>
+          <li>Search</li>
+          <li>About Me</li>
+          <li>List</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Navbar;
