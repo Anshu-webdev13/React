1e0b66260e83c51a946c7472012a9435e3300edc
Anshu kumari <anshuak6595@gmail.com>
2026-08-06 22:09:54 +0530

    Add this Folder

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Learning React</h1>
+      <p>React is a library of Javascript</p>
+      <p>I am learning javascript</p>
+    </>
+  );
+}
+export default App;
