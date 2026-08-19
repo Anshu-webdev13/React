495942515453647d0e95b88fe081cbf3145217df
Anshu kumari <anshuak6595@gmail.com>
2026-08-19 23:53:06 +0530

    Add updated code

--- 11-Note-app/src/App.jsx
+++ 11-Note-app/src/App.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(title);
+    setTitle("");
+    setDetail("");
   };
   return (
     <div className="h-screen lg:flex bg-black text-white ">
@@ -17,18 +22,26 @@ const App = () => {
           type="text"
           placeholder="Enter the notes heading"
           className="flex px-5 py-2  w-full border-2 font-medium outline-none rounded"
+          value={tittle}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
         <textarea
           type="text"
           placeholder="Enter the details of notes"
           className="flex px-5 py-2 h-32 flex-row w-full font-medium border-2 outline-none rounded items-start"
+          value={detail}
+          onChange={(e) => {
+            setDetail(e.target.value);
+          }}
         />
         <button className="flex px-5 py-2 flex-row w-full font-medium  bg-white text-black  border-2 outline-none rounded text-center">
           Add Notes
         </button>
       </form>
       <div className="lg:w-1/2 p-10 lg:border-l-2">
-        <h1 className="text-3xl font-bold">Your Notes</h1>
+        <h1 className="text-3xl font-bold">Recent Notes</h1>
         <div className="flex flex-wrap gap-5 mt-5 overflow-auto h-full">
           <div className="h-52 w-40 bg-white rounded-2xl"></div>
           <div className="h-52 w-40 bg-white rounded-2xl"></div>
