ad11b905ba19e6c0a7491dae45233524001d3075
Anshu kumari <anshuak6595@gmail.com>
2026-09-02 23:12:24 +0530

    Add advance routing folder

---
+++ 18-Advance-routing/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>Seriyans</h2>
+      <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav
